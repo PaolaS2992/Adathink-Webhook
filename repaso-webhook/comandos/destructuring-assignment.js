7438210d49664c453a4a5cab48d2f22a8c32693c
PaolaS2992 <huaripayta29.92@gmail.com>
2020-05-02 18:27:06 -0500

    repaso clase - for-in-of | ref-clonar-obj | destructuring-assignment

---
+++ repaso-webhook/comandos/destructuring-assignment.js
@@ -0,0 +1,26 @@
+let paises = ["Peru","Colombia","Alemania","Uruguay"];
+let usuario = {"nombre":"Andy Javier","edad":21};
+let colores= [{"nombre":"Verde"},{"nombre":"Rojo"},{"nombre":"Azul"}];
+
+
+//Extraer datos 
+
+//ARRAY 
+//Antigua 
+//Peru - Alemania
+// let peru = paises[0], alemania = paises[2];
+// console.log(peru,alemania)
+
+
+// let [peru , , ...uru ] = paises ;
+// console.log(peru,uru)
+
+
+// // Objetos extrae variables referenciales
+// let {nombre:name,edad} = usuario;
+// console.log(name,edad);
+
+
+// nombre = "Andy Javier Reyes";
+// console.log(usuario)
+
